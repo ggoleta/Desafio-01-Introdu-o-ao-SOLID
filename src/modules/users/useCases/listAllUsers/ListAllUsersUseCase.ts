cd00ac9bb2a03bcf75476b4183a3cbf078c6a779
ggoleta <rogerio_contato@hotmail.com>
2021-04-17 23:37:24 -0300

    challenge complete

--- src/modules/users/useCases/listAllUsers/ListAllUsersUseCase.ts
+++ src/modules/users/useCases/listAllUsers/ListAllUsersUseCase.ts
@@ -2,14 +2,24 @@ import { User } from "../../model/User";
 import { IUsersRepository } from "../../repositories/IUsersRepository";
 
 interface IRequest {
-  user_id: string;
+  user_id: string | string[];
 }
 
 class ListAllUsersUseCase {
   constructor(private usersRepository: IUsersRepository) {}
 
   execute({ user_id }: IRequest): User[] {
-    // Complete aqui
+    const user = this.usersRepository.findById(user_id.toString());
+
+    if (!user) {
+      throw new Error("User isn't a admin.");
+    }
+
+    if (!user.admin) {
+      throw new Error("User isn't a admin.");
+    }
+    const users = this.usersRepository.list();
+    return users;
   }
 }
 
